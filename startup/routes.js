f61c37c1dd52a2c9fc6694d21a2655c13e21f368
nascal3 <nascal3@gmail.com>
2019-01-19 08:42:22 +0300

    create new route for area radius

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const promo = require('../routes/promo_codes');
+const radius = require('../routes/radius');
 
 module.exports = function (app) {
   app.use(express.json());
   app.use('/api/promocode', promo);
+  app.use('/api/radius', radius);
 };
