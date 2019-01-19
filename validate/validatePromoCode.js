ae0e2ee8d5ba22a431f843b6754c81528ff600d8
nascal3 <nascal3@gmail.com>
2019-01-19 12:35:10 +0300

    created a function to test and validate usability of the promotion code

---
+++ validate/validatePromoCode.js
@@ -0,0 +1,28 @@
+const inside = require('point-in-polygon');
+const Promo = require('../models/promo_code');
+const Area = require('../models/area');
+require('express-async-errors');
+
+const validateCode = (async (promoCode, start, destination) => {
+
+  const exist = await Promo.findAll({
+    where: {
+      code: promoCode
+    },
+    include: [Area]
+  });
+
+  if (exist.length < 1) return "Code provided does not exist :(";
+  if (exist[0].state === 'inactive') return "Code provided is not usable :(";
+  if (exist[0].exp_date < new Date()) return "Code provided has expired :(";
+
+  const areaRadius = JSON.parse(exist[0].area.radius);
+  const startInRange = inside(start, areaRadius);
+  const destinationInRange = inside(destination, areaRadius);
+  if (!startInRange || !destinationInRange) return "Your location and destination are out of promotion range :(";
+
+  return exist;
+
+});
+
+module.exports = validateCode;
