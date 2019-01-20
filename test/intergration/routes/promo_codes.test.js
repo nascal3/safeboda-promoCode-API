908df02d3e635d6e5df920dae058cf4e2fc72ab4
nascal3 <nascal3@gmail.com>
2019-01-21 00:14:45 +0300

    added test for promo code validation.

--- test/intergration/routes/promo_codes.test.js
+++ test/intergration/routes/promo_codes.test.js
@@ -1,19 +1,26 @@
 const request = require('supertest');
 const Promo = require("../../../models/promo_code");
+const Area = require("../../../models/area");
 let server;
 
 describe('/api/promocode', () => {
   beforeEach(() => {
     server = require('../../../app');
+  });
+
+  afterEach(() => {
+    server.close();
     Promo.destroy({
       where: {
         code: "code"
       }
     });
-  });
 
-  afterEach(() => {
-    server.close();
+    Area.destroy({
+      where: {
+        area_name: "test area"
+      }
+    });
   });
 
   describe('GET', () => {
@@ -31,7 +38,33 @@ describe('/api/promocode', () => {
       const res = await request(server).get('/api/promocode/all');
       expect(res.status).toBe(200);
       expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
 
+  describe('GET', () => {
+    it('should return valid promotion code info', async () => {
+
+      const exp_date = new Date(new Date().getTime() + (5 * 24 * 60 * 60 * 1000));
+      await Promo.create(
+          {
+            code: 'code',
+            amount: 200,
+            exp_date: exp_date
+          }
+      );
+
+      const radius_area = [[ 1, 1 ], [ 1, 2 ], [ 2, 2 ], [ 2, 1 ]];
+      await Area.create({
+        area_name: "test area",
+        radius: JSON.stringify(radius_area)
+      });
+
+      const res = await request(server).get('/api/promocode/validate', {
+        "code": "testCode",
+        "start": [  1.5, 1.5 ],
+        "destination": [  1.8, 1.1  ]
+      });
+      expect(res.body).toHaveProperty('code', res.code);
     });
   })
 
