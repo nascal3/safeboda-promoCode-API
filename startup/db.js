ef10cd092ac4f79e8bfbfa8887f0c9077416099f
nascal3 <nascal3@gmail.com>
2019-06-03 22:33:19 +0300

    hard code vars

--- startup/db.js
+++ startup/db.js
@@ -2,13 +2,16 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DATABASE,
-    process.env.USER,
-    process.env.PASSWORD,
+    // process.env.DATABASE,
+    // process.env.USER,
+    // process.env.PASSWORD,
+    'kodipoin_node',
+    'kodipoin_node',
+    '3kFYVT4WmUUT',
     {
       dialect: 'mysql',
-      // host: 'localhost',
-      host: process.env.HOST,
+      host: 'localhost',
+      // host: process.env.HOST,
       operatorsAliases: false
     }
 );
