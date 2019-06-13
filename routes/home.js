1be0bebf1d0cd8839caea31057cdbfdc61f5373b
nascal3 <nascal3@gmail.com>
2019-06-13 23:49:27 +0300

    Change healthy check message

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const Promo = require('../models/promo_code');
-const Area = require('../models/area');
 require('express-async-errors');
 const router = express.Router();
 
 // GET all promo code listing
 router.get('/', async (req, res) => {
-
-    const allCodes = await Promo.findAll({
-        include: [Area]
-    });
-
-    res.status(200).send(allCodes);
+     res.status(200).send("server is healthy");
 });
 
 module.exports = router;
